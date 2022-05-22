c255ec8fb49ede869574e809838f6ffca4b57534
squareman77 <75134215+squareman77@users.noreply.github.com>
2022-05-21 23:46:26 -0400

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -11,7 +11,7 @@ export default () => {
         <div className="mx-auto text-center my-16">
             <h1 className="inline-block text-6xl py-2 px-4 rounded-lg font-semibold bg-white border-4 border-solid border-gray-300">Test successfully submitted.</h1>
             <br/>
-            <p className="inline-block my-8 bg-white rounded-lg border-4 border-solid border-gray-300 py-2 px-4 text-3xl">Yay! Results will be released soon!</p>
+            <p className="inline-block my-8 bg-white rounded-lg border-4 border-solid border-gray-300 py-2 px-4 text-3xl">Thank you for your submission. Results will be released soon.</p>
         </div>
     </>
 }
